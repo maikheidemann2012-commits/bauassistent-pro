5746378509a3cfa17238abfe82be97c58709a983
Maik Heidemann <maikheidemann2012@gmail.com>
2026-05-11 07:02:33 +0200

    Clerk entfernt

--- app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-[#080c12]">
-      <SignIn />
-    </main>
-  );
-}
